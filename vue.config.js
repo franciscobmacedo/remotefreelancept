daf96cc2efa2f1289b95509135bd98916524b669
Francisco Macedo <francisco.macedo@optimile.eu>
2021-08-22 18:15:59 +0100

    [feature] added URL config settings

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  transpileDependencies: [
-    'vuetify'
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/remoteSimulatorPT/'
+    : '/',
+  "transpileDependencies": [
+    "vuetify"
   ]
 }
