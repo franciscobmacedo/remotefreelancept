07ac47177606599e2a5c5570b08960762a72bfb8
Francisco Macedo <francisco.macedo@optimile.eu>
2021-08-22 17:57:40 +0100

    [feature] adapt currency

--- src/utils.js
+++ src/utils.js
@@ -2,11 +2,24 @@ export function round(num, decimal = 0){
     return parseFloat(num).toFixed(decimal)
   }
 
-function formatNumber (num) {
+export function formatNumber (num) {
     return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ")
 }
+
+
+export function currencyNoSymbol(num, decimalPlaces=0){
+    return formatNumber(parseFloat(num).toFixed(decimalPlaces))
+}
+
 export function currency(num, decimalPlaces=0){
-    return formatNumber(parseFloat(num).toFixed(decimalPlaces)).toString() + "€"
+    return currencyNoSymbol(num, decimalPlaces) + "€"
+}
+
+
+export function reverseCurrency(num){
+     
+    const result = parseFloat(num.replaceAll(' ','').replaceAll('€', ''))
+    return result <= 0 ? NaN:result
 }
 
 export const frequencyItems = {
