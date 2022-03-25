751021e2ac8fff89ff567e7407ed190cdaa4bc0f
Francisco Macedo <francisco.macedo@optimile.eu>
2022-03-25 12:04:11 +0000

    update production url config

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/remoteSimulatorPT/'
-    : '/',
-  "transpileDependencies": [
-    "vuetify"
-  ]
-}
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/remotefreelancept/" : "/",
+  transpileDependencies: ["vuetify"],
+};
