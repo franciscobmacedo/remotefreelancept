fae8146e9748f4f3f8f9af58d98bd7e1e4754f37
franciscobmacedo <franciscovcbm@gmail.com>
2023-03-26 23:49:31 +0100

    use heroicons over custom svg

--- src/components/TaxRanksDialog.vue
+++ src/components/TaxRanksDialog.vue
@@ -51,19 +51,7 @@
             data-modal-hide="defaultModal"
             @click="$emit('close')"
           >
-            <svg
-              aria-hidden="true"
-              class="w-5 h-5"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
+            <XMarkIcon class="w-5 h-5" />
             <span class="sr-only">Close modal</span>
           </button>
         </div>
@@ -122,10 +110,9 @@ import { useTaxesStore } from "@/stores";
 import { asCurrency, asPercentage } from "@/utils";
 import { computed } from "vue";
 import { storeToRefs } from "pinia";
-
+import { XMarkIcon } from "@heroicons/vue/24/outline";
 // store
 const { taxRanks, taxRank, taxableIncome } = storeToRefs(useTaxesStore());
-
 
 // taxRank
 const taxRankMinText = computed(() => {
