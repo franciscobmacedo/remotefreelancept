9c5fc785d9451f8116bef0a57c06454109d089e7
Francisco Macedo <franciscovcbm@gmail.com>
2024-01-21 18:52:41 +0000

    add page header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
+  <PageHeader></PageHeader>
   <main>
     <RouterView />
   </main>
 </template>
+<script lang="ts" setup>
+import PageHeader from "@/components/PageHeader.vue";
+</script>
