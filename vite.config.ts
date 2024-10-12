a5807c2fbf7cfa39d65c9f18c3588c4c89c75df6
Igor Ferreira Pinto <fp.igor@hotmail.com>
2024-10-12 21:52:40 +0100

    update vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [vue()],
-  base: "/remotefreelancept/",
+  base: "/remotefreelancept/", //repo name
   server: {
     port: process.env.PORT || null
   },
